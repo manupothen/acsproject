2759abe81990f4da8296bc12403bbb39668f6641
EC2 Default User <ec2-user@ip-172-31-64-114.ec2.internal>
2022-04-14 06:50:10 +0000

    global var file

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "project"
+    "App"   = "Web"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Group25"
+}
