154fcc5e8078a51a82f08cf627417076017a09e9
manupothen <102735434+manupothen@users.noreply.github.com>
2022-04-16 00:51:18 +0530

    Update config.tf

--- env/staging/network/config.tf
+++ env/staging/network/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "staging-acsproject"
+    bucket = "staging-acsproject-group25"
     key    = "staging-network/terraform.tfstate"
     region = "us-east-1"
   }
