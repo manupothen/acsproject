e83cbc033b3628ca34de3ece902aab67b8cf13a8
manupothen <102735434+manupothen@users.noreply.github.com>
2022-04-16 01:10:55 +0530

    Update install_httpd.sh

--- env/prod/webservers/install_httpd.sh
+++ env/prod/webservers/install_httpd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
-sudo aws s3 cp s3://prod-acsproject/project.jpg /var/www/html/
-sudo aws s3 cp s3://prod-acsproject/project2.jpg /var/www/html/
+sudo aws s3 cp s3://prod-acsproject-group25/project.jpg /var/www/html/
+sudo aws s3 cp s3://prod-acsproject-group25/project2.jpg /var/www/html/
 echo '<h1>Group 25. Group members: Manu Pothen, Rosemary Sebastian, Bibin Mekkattukunnil Biju</h1> <img src=project.jpg> <img src=project2.jpg>'   >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
